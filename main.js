f3e7ad8306e722d3e1a9e18ed9e2825bd9d8a6ed
Vephla University <117038379+VephlaUniversity@users.noreply.github.com>
2024-05-03 21:22:11 +0100

    final commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function mouseOver() {
+    document.getElementById("visible").style.display = "block";
+}
+function mouseOut() {
+    document.getElementById("visible").style.display = "none";
+}
+function hoverOn() {
+    document.getElementById("visible-one").style.display = "block";
+}
+function hoverOut() {
+    document.getElementById("visible-one").style.display = "none";
+}
